f948547c3011aa034ae5118bfbf73b0ea9133995
Matt <matt@sao.network>
2022-11-07 12:25:38 +0800

    update meta

--- x/model/types/errors.go
+++ x/model/types/errors.go
@@ -11,4 +11,5 @@ var (
 	ErrDataIdExists  = sdkerrors.Register(ModuleName, 4100, "dataId already exists")
 	ErrInvalidDataId = sdkerrors.Register(ModuleName, 4101, "invali dataId")
 	ErrModelExists   = sdkerrors.Register(ModuleName, 4102, "model already exists")
+	ErrOnlyOwner     = sdkerrors.Register(ModuleName, 4103, "owner only action")
 )
