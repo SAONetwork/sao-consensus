9ee8d0f106a0425954c1c954be3ac2cc3a4d44ca
lyj <lyj@sao.network>
2022-11-18 16:11:42 +0800

    add map DidBindingProofs

--- x/did/types/genesis_test.go
+++ x/did/types/genesis_test.go
@@ -19,12 +19,34 @@ func TestGenesisState_Validate(t *testing.T) {
 			valid:    true,
 		},
 		{
-			desc:     "valid genesis state",
+			desc: "valid genesis state",
 			genState: &types.GenesisState{
 
+				DidBindingProofsList: []types.DidBindingProofs{
+					{
+						AccountId: "0",
+					},
+					{
+						AccountId: "1",
+					},
+				},
 				// this line is used by starport scaffolding # types/genesis/validField
 			},
 			valid: true,
+		},
+		{
+			desc: "duplicated didBindingProofs",
+			genState: &types.GenesisState{
+				DidBindingProofsList: []types.DidBindingProofs{
+					{
+						AccountId: "0",
+					},
+					{
+						AccountId: "0",
+					},
+				},
+			},
+			valid: false,
 		},
 		// this line is used by starport scaffolding # types/genesis/testcase
 	} {
