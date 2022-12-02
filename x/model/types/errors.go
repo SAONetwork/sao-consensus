39a7aff7a1c63cd725c1520c223239895f818472
Not@All <well@sao.network>
2022-12-02 19:30:43 +0800

    fix integration issue

--- x/model/types/errors.go
+++ x/model/types/errors.go
@@ -13,4 +13,5 @@ var (
 	ErrModelExists      = sdkerrors.Register(ModuleName, 4102, "model already exists")
 	ErrOnlyOwner        = sdkerrors.Register(ModuleName, 4103, "owner only action")
 	ErrInsufficientCoin = sdkerrors.Register(ModuleName, 4104, "insufficient coin")
+	ErrInvalidOperation = sdkerrors.Register(ModuleName, 4105, "invalid operation")
 )
