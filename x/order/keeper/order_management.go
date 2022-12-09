8602947e02d3a1d29b1a82ecfd002294823e51d7
Not@All <well@sao.network>
2022-12-09 16:49:41 +0800

    fix duration issue

--- x/order/keeper/order_management.go
+++ x/order/keeper/order_management.go
@@ -19,7 +19,7 @@ func (k Keeper) NewOrder(ctx sdk.Context, order types.Order, sps []string) (uint
 
 	logger := k.Logger(ctx)
 
-	logger.Debug("######try payment", "payer", paymentAcc, "amount", order.Amount)
+	logger.Error("######try payment", "payer", paymentAcc, "amount", order.Amount)
 
 	err = k.bank.SendCoinsFromAccountToModule(ctx, paymentAcc, types.ModuleName, sdk.Coins{order.Amount})
 	if err != nil {
