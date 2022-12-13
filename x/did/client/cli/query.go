055df50abeacbd2270cbb2bb073986334109885f
sterlynLee <sterlyn@sao.network>
2022-12-13 12:35:08 +0800

    add did query ValidateDid

--- x/did/client/cli/query.go
+++ x/did/client/cli/query.go
@@ -41,6 +41,8 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	cmd.AddCommand(CmdShowPastSeeds())
 	cmd.AddCommand(CmdListPaymentAddress())
 	cmd.AddCommand(CmdShowPaymentAddress())
+	cmd.AddCommand(CmdValidateDid())
+
 	// this line is used by starport scaffolding # 1
 
 	return cmd
