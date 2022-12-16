7522c844c67342f06ea27bd812a7c19b1b5d8b1b
sterlynLee <sterlyn@sao.network>
2022-12-16 16:40:20 +0800

    update ts client builder

--- ts-client-builder/entry.ts
+++ ts-client-builder/entry.ts
@@ -1,17 +1,20 @@
-import {queryClient as didQueryClient} from "./saonetwork.sao.did"
-import {queryClient as nodeQueryClient} from "./saonetwork.sao.node"
-import {queryClient as saoQueryClient} from "./saonetwork.sao.sao"
+export * from "./index"
 
-export {Client} from "./index"
-export {MsgUpdateSidDocumentResponse} from "./saonetwork.sao.did/types/sao/did/tx"
-export {
-    Proposal,
-    QueryProposal,
-    RenewProposal,
-    PermissionProposal,
-    TerminateProposal
-} from "./saonetwork.sao.sao"
+// export proto types
+export * as SaoTypes from "./saonetwork.sao.sao"
+export * as DidTypes from "./saonetwork.sao.did"
+export * as NodeTypes from "./saonetwork.sao.node"
+export * as ModelTypes from "./saonetwork.sao.model"
+export * as OrderTypes from "./saonetwork.sao.order"
+export * as MarketTypes from "./saonetwork.sao.market"
+
+// export tx types
+export * as DidTxTypes from "./saonetwork.sao.did/types/sao/did/tx"
+export * as SaoTxTypes from "./saonetwork.sao.sao/types/sao/sao/tx"
+export * as NodeTxTypes from "./saonetwork.sao.node/types/sao/node/tx"
+export * as ModelTxTypes from "./saonetwork.sao.model/types/sao/model/tx"
+export * as OrderTxTypes from "./saonetwork.sao.order/types/sao/order/tx"
+export * as MarketTxTypes from "./saonetwork.sao.market/types/sao/market/tx"
+
 export {Api} from "./saonetwork.sao.sao/rest"
-export {MsgStoreResponse} from "./saonetwork.sao.sao/types/sao/sao/tx"
 export {TxMsgData} from "./cosmos.tx.v1beta1/types/cosmos/base/abci/v1beta1/abci";
-export {nodeQueryClient, didQueryClient, saoQueryClient}
