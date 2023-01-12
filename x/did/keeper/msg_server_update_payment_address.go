3ed3e1d4098ac5cff9998c7d5eb0e8af57a70f63
sterlynLee <sterlyn@sao.network>
2023-01-12 10:18:18 +0800

    add error log

--- x/did/keeper/msg_server_update_payment_address.go
+++ x/did/keeper/msg_server_update_payment_address.go
@@ -11,21 +11,26 @@ import (
 
 func (k msgServer) UpdatePaymentAddress(goCtx context.Context, msg *types.MsgUpdatePaymentAddress) (*types.MsgUpdatePaymentAddressResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	logger := k.Logger(ctx)
+	k.CheckCreator(ctx, msg.Creator, msg.Did)
 	accId := msg.GetAccountId()
 	accIdSplits := strings.Split(accId, ":")
 	if len(accIdSplits) == 3 && accIdSplits[0] == "cosmos" && accIdSplits[1] == ctx.ChainID() {
 		// err if did is a key did or empty, which means update payment address for sid
 		did, err := saodidparser.Parse(msg.Did)
 		if err != nil {
+			logger.Error("failed to parse did %v", msg.Did)
 			return nil, types.ErrInvalidDid
 		}
 		switch did.Method {
 		case "sid":
 			proof, found := k.GetDidBindingProof(ctx, accId)
 			if !found {
+				logger.Error("account id %v has not bound to did %v yet", msg.AccountId, msg.Did)
 				return nil, types.ErrBindingNotFound
 			}
 			if msg.Did != proof.Proof.Did {
+				logger.Error("did %v in binding proof is different to did %v in message", proof.Proof.Did, msg.Did)
 				return nil, types.ErrInconsistentDid
 			}
 
@@ -41,9 +46,11 @@ func (k msgServer) UpdatePaymentAddress(goCtx context.Context, msg *types.MsgUpd
 			}
 			k.SetPaymentAddress(ctx, paymentAddress)
 		default:
+			logger.Error("unsupported did %v", msg.Did)
 			return nil, types.ErrUnsupportedDid
 		}
 	} else {
+		logger.Error("you should use a cosmos account in %v chain, but we get %v ", ctx.ChainID(), msg.AccountId)
 		return nil, types.ErrInvalidAccountId
 	}
 
