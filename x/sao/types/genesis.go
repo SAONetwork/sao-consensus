599e682a5229b2714077ca2b507ea799ffd46c79
sterlynLee <sterlyn@sao.network>
2023-03-27 11:43:51 +0800

    add ignite scarffold placeholder

--- x/sao/types/genesis.go
+++ x/sao/types/genesis.go
@@ -1,5 +1,7 @@
 package types
 
+// this line is used by starport scaffolding # genesis/types/import
+
 // DefaultIndex is the default global index
 const DefaultIndex uint64 = 1
 
@@ -14,5 +16,8 @@ func DefaultGenesis() *GenesisState {
 // Validate performs basic genesis state validation returning an error upon any
 // failure.
 func (gs GenesisState) Validate() error {
+
+	// this line is used by starport scaffolding # genesis/types/validate
+
 	return gs.Params.Validate()
 }
