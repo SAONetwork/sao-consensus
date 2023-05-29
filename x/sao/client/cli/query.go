ae6faf1d64363219e6f97a97043dbc46b4b7b528
Not@All <well@sao.network>
2023-05-29 18:36:02 +0800

    add net version query

--- x/sao/client/cli/query.go
+++ x/sao/client/cli/query.go
@@ -33,6 +33,8 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	cmd.AddCommand(CmdShowTimeoutOrder())
 	cmd.AddCommand(CmdListExpiredShard())
 	cmd.AddCommand(CmdShowExpiredShard())
+	cmd.AddCommand(CmdNetVersion())
+
 	// this line is used by starport scaffolding # 1
 
 	return cmd
---
+++ x/sao/client/cli/query_net_version.go
@@ -0,0 +1,42 @@
+package cli
+
+import (
+	"strconv"
+
+	"github.com/SaoNetwork/sao/x/sao/types"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+)
+
+var _ = strconv.Itoa(0)
+
+func CmdNetVersion() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "net-version",
+		Short: "Query NetVersion",
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			params := &types.QueryNetVersionRequest{}
+
+			res, err := queryClient.NetVersion(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
